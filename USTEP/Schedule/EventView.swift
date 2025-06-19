4c97dfc318a70d3c63e0db8289d7a4348c9ba8db
Jonah Brooks <Brooks.Jonah.C@gmail.com>
2025-06-19 14:19:27 -0600

    Calculating the frailty score; still not displaying it yet.

--- USTEP/Schedule/EventView.swift
+++ USTEP/Schedule/EventView.swift
@@ -6,6 +6,8 @@
 // SPDX-License-Identifier: MIT
 //
 
+// swiftlint:disable closure_body_length
+import ModelsR4
 import SpeziQuestionnaire
 import SpeziScheduler
 import SwiftUI
@@ -20,14 +22,49 @@ struct EventView: View {
     var body: some View {
         if let compoundQuestionnaire = event.task.compoundQuestionnaire {
             CompoundQuestionnaireView(compoundQuestionnaire: compoundQuestionnaire) { result in
+                
                 dismiss()
-
+                
                 guard case let .completed(response) = result else {
                     return // user cancelled the task
                 }
-
+                
                 event.complete()
                 await standard.add(response: response)
+                
+                var edmontonScore = 0
+                if let answers = response.item {
+                    var score: Int = 0
+                    for answer in answers {
+                        if answer.linkId.value?.string != nil && answer.linkId.value!.string.starts(with: "Edmonton") {
+                            if answer.answer != nil && !answer.answer!.isEmpty {
+                                if let value = answer.answer![0].value {
+                                    var answerScore: Int = 0
+                                    if case let .coding(codingData) = value {
+                                        if let codingScore: Int = Int(codingData.code?.value?.string ?? "0") {
+                                            answerScore = codingScore
+                                        }
+                                    } else if case let .string(stringValue) = value {
+                                        if let stringScore: Int = Int(stringValue.value?.string ?? "0") {
+                                            answerScore = stringScore
+                                        }
+                                    } else {
+                                        // No score present for this step
+                                    }
+                                    score += answerScore
+                                }
+                            }
+                        }
+                    }
+                    edmontonScore = score
+                }
+                let healthCategories = ["Healthy", "Vulnerable", "Frail"]
+                var category = healthCategories[0]
+                if edmontonScore > 10 {
+                    category = healthCategories[2]
+                } else if edmontonScore > 5 {
+                    category = healthCategories[1]
+                }
             }
         } else {
             NavigationStack {
